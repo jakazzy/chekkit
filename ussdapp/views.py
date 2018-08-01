b313c3e523b76955f733278ad49a1bc67d4b840a
jida <jakazzy@gmail.com>
2018-08-01 17:09:53 +0000

    refactor: added options to check_it view function

--- ussdapp/views.py
+++ ussdapp/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from ussdapp.processor import USSDProcessor
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'OPTIONS'])
 def check_it(request):
     data = request.data
     processor = USSDProcessor(data=data)
