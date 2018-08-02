0bdfa5d9cf27e9361f5b1e0db518d7eb46aad40b
jida <jakazzy@gmail.com>
2018-08-02 05:47:19 +0000

    fix(ussdapp): config

--- ussdapp/views.py
+++ ussdapp/views.py
@@ -1,10 +1,15 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from ussdapp.processor import USSDProcessor
 
 
-@api_view(['GET', 'POST', 'OPTIONS'])
+@api_view(['OPTIONS', 'GET', 'POST'])
 def check_it(request):
-    data = request.data
-    processor = USSDProcessor(data=data)
-    return processor.process_request()
+    if request.method == 'OPTIONS':
+        return Response({}, status=status.HTTP_200_OK)
+    else:
+        data = request.data
+        processor = USSDProcessor(data=data)
+        return processor.process_request()
